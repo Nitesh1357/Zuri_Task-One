c4b8286bbe174ba646e55d6e95027e8a30c9ba3e
Nitesh Kumar Mishra <104729540+Nitesh1357@users.noreply.github.com>
2023-09-09 15:29:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const dataDay = document.querySelector("[data-testid='currentDayOfTheWeek']");
+const dataTime = document.querySelector("[data-testid='currentUTCTime']");
+
+console.log(dataDay)
+function updateUTCTime() {
+    const time = new Date().getTime();
+    dataTime.textContent = time
+}
+function updateUTCDay() {
+    const options = {
+        timeZone: 'UTC',
+        weekday: 'long',
+    };
+    const day = new Date().toLocaleDateString('en-US', options);
+    dataDay.textContent = day
+}
+// Update the time initially
+updateUTCTime();
+updateUTCDay();
+
+setInterval(updateUTCTime, 1000);
